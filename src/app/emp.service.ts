b60b6c83b480f01ec1de5538aa5ad16c45acd2c7
cristina1084 <cristina.johns1084@gmail.com>
2019-10-07 21:44:50 +0530

    first commit

--- src/app/emp.service.ts
+++ src/app/emp.service.ts
@@ -9,27 +9,27 @@ export class EmpService {
   constructor(private http: HttpClient) { }
 
   addEmp(emp){
-    return this.http.post("http://localhost:8080/emp/addemp", emp)
+    return this.http.post("https://empserver.herokuapp.com/emp/addemp", emp)
   }
 
   getEmp(){
-    return this.http.get("http://localhost:8080/emp/getemp")
+    return this.http.get("https://empserver.herokuapp.com/emp/getemp")
   }
 
   getEmp1(eid){
-    return this.http.get("http://localhost:8080/emp/getemp/"+eid)
+    return this.http.get("https://empserver.herokuapp.com/emp/getemp/"+eid)
   }
 
   editEmp(emp){
-    return this.http.post("http://localhost:8080/emp/editemp", emp)
+    return this.http.post("https://empserver.herokuapp.com/emp/editemp", emp)
   }
 
   deleteEmp(eid){
-    return this.http.get("http://localhost:8080/emp/deleteemp/"+eid)
+    return this.http.get("https://empserver.herokuapp.com/emp/deleteemp/"+eid)
   }
 
   login(uname,pwd){
     var log = {username: uname, password: pwd}
-    return this.http.post("http://localhost:8080/login", log)
+    return this.http.post("https://empserver.herokuapp.com/login", log)
   }
 }
